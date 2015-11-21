01930b8fd83ff1e69ea2a7be7cc15610755fb4b3
alexluan <326083325@qq.com>
2015-11-22 02:42:03 +0800

    Initial Commit

---
+++ MyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProject
+//
+//  Created by 栾有数 on 15/11/22.
+//  Copyright © 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyProject
+//
+//  Created by 栾有数 on 15/11/22.
+//  Copyright © 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
